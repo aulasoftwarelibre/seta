6ba27aadc06d70b50563d25b65c7b48157f01409
Sergio Gomez <sergio@uco.es>
2016-02-17 06:45:35 +0100

    Muestra la lista de alquileres de una taquilla (closes #5)

--- src/Seta/CoreBundle/DependencyInjection/SetaCoreExtension.php
+++ src/Seta/CoreBundle/DependencyInjection/SetaCoreExtension.php
@@ -29,5 +29,6 @@ class SetaCoreExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+        $loader->load('twig.xml');
     }
 }
